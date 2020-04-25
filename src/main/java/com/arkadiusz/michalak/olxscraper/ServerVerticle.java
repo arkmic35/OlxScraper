0cbdba440e4b5b30fd8c0ded8ce52ee1742fdcf0
Arkadiusz Michalak <arkmic35@gmail.com>
2020-04-25 17:48:17 +0200

    Added JSON serialization to offers endpoint

--- src/main/java/com/arkadiusz/michalak/olxscraper/ServerVerticle.java
+++ src/main/java/com/arkadiusz/michalak/olxscraper/ServerVerticle.java
@@ -1,9 +1,12 @@
 package com.arkadiusz.michalak.olxscraper;
 
+import com.arkadiusz.michalak.olxscraper.model.ScrapingResponse;
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.http.HttpMethod;
+import io.vertx.core.json.Json;
 import io.vertx.ext.web.Router;
 
+import java.util.Arrays;
 import java.util.Random;
 
 public class ServerVerticle extends AbstractVerticle {
@@ -14,21 +17,30 @@ public class ServerVerticle extends AbstractVerticle {
     @Override
     public void start() {
         Router router = Router.router(vertx);
-
-        router.route(HttpMethod.GET, "/offers/olx/:keyword")
-                .handler(routingContext -> {
-                    String keyword = routingContext.request().getParam("keyword");
-
-                    routingContext
-                            .response()
-                            .putHeader("content-type", "text/json")
-                            .end("Looking for " + keyword + " in OLX.");
-                });
+        addOlxOffersEndpointTo(router);
 
         vertx.createHttpServer()
                 .requestHandler(router)
                 .listen(PORT_NUMBER);
 
         System.out.println("Server started on port " + PORT_NUMBER);
+    }
+
+    private void addOlxOffersEndpointTo(Router router) {
+        router.route(HttpMethod.GET, "/offers/olx/:keyword")
+                .produces("text/json")
+                .handler(routingContext -> {
+                    String keyword = routingContext.request().getParam("keyword");
+
+                    ScrapingResponse response = ScrapingResponse.of(
+                            Arrays.asList(
+                                    ScrapingResponse.ResponseSegment.of("1", "VW Passat zdrowy", "7000 PLN"),
+                                    ScrapingResponse.ResponseSegment.of("2", "VW Passat niebity igła", "8500 PLN")
+                            )
+                    );
+
+                    routingContext.response()
+                            .end(Json.encodeToBuffer(response));
+                });
     }
 }
