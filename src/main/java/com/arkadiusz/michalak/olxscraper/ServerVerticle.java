12f13024df1215fae4c01c274017088e56e83cb4
Arkadiusz Michalak <arkmic35@gmail.com>
2020-04-25 17:54:28 +0200

    Small refactor of routing mechanism

--- src/main/java/com/arkadiusz/michalak/olxscraper/ServerVerticle.java
+++ src/main/java/com/arkadiusz/michalak/olxscraper/ServerVerticle.java
@@ -2,9 +2,9 @@ package com.arkadiusz.michalak.olxscraper;
 
 import com.arkadiusz.michalak.olxscraper.model.ScrapingResponse;
 import io.vertx.core.AbstractVerticle;
-import io.vertx.core.http.HttpMethod;
 import io.vertx.core.json.Json;
 import io.vertx.ext.web.Router;
+import io.vertx.ext.web.RoutingContext;
 
 import java.util.Arrays;
 import java.util.Random;
@@ -17,7 +17,7 @@ public class ServerVerticle extends AbstractVerticle {
     @Override
     public void start() {
         Router router = Router.router(vertx);
-        addOlxOffersEndpointTo(router);
+        router.get("/offers/olx/:keyword").handler(this::getOlxOffersHandler);
 
         vertx.createHttpServer()
                 .requestHandler(router)
@@ -26,21 +26,18 @@ public class ServerVerticle extends AbstractVerticle {
         System.out.println("Server started on port " + PORT_NUMBER);
     }
 
-    private void addOlxOffersEndpointTo(Router router) {
-        router.route(HttpMethod.GET, "/offers/olx/:keyword")
-                .produces("text/json")
-                .handler(routingContext -> {
-                    String keyword = routingContext.request().getParam("keyword");
+    private void getOlxOffersHandler(RoutingContext context) {
+        String keyword = context.request().getParam("keyword");
 
-                    ScrapingResponse response = ScrapingResponse.of(
-                            Arrays.asList(
-                                    ScrapingResponse.ResponseSegment.of("1", "VW Passat zdrowy", "7000 PLN"),
-                                    ScrapingResponse.ResponseSegment.of("2", "VW Passat niebity igła", "8500 PLN")
-                            )
-                    );
+        ScrapingResponse response = ScrapingResponse.of(
+                Arrays.asList(
+                        ScrapingResponse.ResponseSegment.of("1", "VW Passat zdrowy", "7000 PLN"),
+                        ScrapingResponse.ResponseSegment.of("2", "VW Passat niebity igła", "8500 PLN")
+                )
+        );
 
-                    routingContext.response()
-                            .end(Json.encodeToBuffer(response));
-                });
+        context.response()
+                .putHeader("Content-Type", "text/json")
+                .end(Json.encodeToBuffer(response));
     }
 }
